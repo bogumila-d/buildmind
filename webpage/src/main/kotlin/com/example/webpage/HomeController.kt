fcc422af6987407d82c04edb9aee1f3c64bb425f
bogumila-d <bodubel@gmail.com>
2026-06-17 22:11:32 +0200

    add_html - add delete action

--- webpage/src/main/kotlin/com/example/webpage/HomeController.kt
+++ webpage/src/main/kotlin/com/example/webpage/HomeController.kt
@@ -6,6 +6,7 @@ import org.springframework.ui.Model
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestParam
+import org.springframework.web.bind.annotation.PathVariable
 import java.time.LocalDateTime
 import java.time.format.DateTimeFormatter
 
@@ -44,6 +45,18 @@ class HomeController(
         val pages = pageService.getAllPages()
         model.addAttribute("pages", pages)
         model.addAttribute("message", "Page created successfully!")
+        return "pages"
+    }
+
+    @PostMapping("/pages/{id}/delete")
+    fun deletePage(
+        @PathVariable id: String,
+        model: Model
+    ): String {
+        pageService.deletePageById(id)
+        val pages = pageService.getAllPages()
+        model.addAttribute("pages", pages)
+        model.addAttribute("message", "Page deleted successfully!")
         return "pages"
     }
 
